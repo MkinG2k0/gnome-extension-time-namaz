069752b8ffb6eac5d17b58a717f9912e43558cae
mk <kamai122000@mail.ru>
2023-08-23 14:01:48 +0300

    add next time

--- extension.js
+++ extension.js
@@ -1,17 +1,55 @@
 const GETTEXT_DOMAIN = 'my-indicator-extension'
 
-const {GObject, St} = imports.gi
+const {GObject, St, Soup, GLib} = imports.gi
 
 const ExtensionUtils = imports.misc.extensionUtils
 const Main = imports.ui.main
 const PanelMenu = imports.ui.panelMenu
 const PopupMenu = imports.ui.popupMenu
 const Clutter = imports.gi.Clutter
+const ByteArray = imports.byteArray
 
 const _ = ExtensionUtils.gettext
-
+const key = '31045ef5d8786c4b194013e79bc9d246'
+const url = (city) => `https://muslimsalat.com/${city}.json?key=${key}`
 // utils
+const fetchTime = async () => {
+	const httpSession = new Soup.Session()
+	const request = Soup.Message.new('GET', url('dagestan'))
 
+	request.request_headers.append('Accept', 'application/json')
+	return await new Promise((resolve, reject) => {
+		httpSession.send_and_read_async(request, GLib.PRIORITY_DEFAULT, null, (httpSession, message) => {
+			const data = ByteArray.toString(httpSession.send_and_read_finish(message).get_data())
+			const obj = JSON.parse(data)
+			resolve(obj)
+		})
+	})
+}
+const getTime = (data, isFullTime = true) => {
+	const arrTime = Object.entries(data.items[0]).filter(([name]) => name !== 'date_for')
+
+	return arrTime.map(([name, fullTime]) => {
+		const [time, type] = fullTime.split(' ')
+		const [hours, minutes] = time.split(':').map(Number)
+		const isFullTimeEnable = isFullTime && type === 'pm'
+		const newHours = isFullTimeEnable ? hours + 12 : hours
+		const newTime = isFullTime ? `${newHours}:${minutes}` : `${hours}:${minutes} ${type}`
+
+		return {name, minutes, hours: newHours, time: newTime}
+	})
+}
+const getNextTime = (times) => {
+	const date = new Date()
+	const nowHours = date.getHours()
+	const nowMinutes = date.getMinutes()
+	const defaultTime = times[0]
+
+	const nextTime = times.find(({hours, minutes}) => {
+		return nowHours <= hours && nowMinutes <= minutes
+	})
+	return nextTime || defaultTime
+}
 //
 
 const Indicator = GObject.registerClass(
@@ -23,17 +61,23 @@ const Indicator = GObject.registerClass(
 
 			this._menuLayout.add(new St.Icon({
 				icon_name: 'org.buddiesofbudgie.Settings-time-symbolic',
+				style_class: 'system-status-icon',
 			}))
 
 			// this._menuLayout.add(new St.Label({
 			// 	text: '\uD83D\uDD4B',  // 🕋, warning
 			// 	y_align: Clutter.ActorAlign.CENTER,
+			// 	style_class: 'system-status-icon',
 			// }))
 
-			this._menuLayout.add(new St.Label({
-				text: 'time',
+			this._timeLabel = new St.Label({
+				text: '00:00',
 				y_align: Clutter.ActorAlign.CENTER,
-			}))
+			})
+
+			this._menuLayout.add(this._timeLabel)
+
+			this.initTime()
 
 			let item = new PopupMenu.PopupMenuItem(_('Show Notification'))
 			item.connect('activate', () => {
@@ -41,6 +85,15 @@ const Indicator = GObject.registerClass(
 			})
 			this.menu.addMenuItem(item)
 		}
+
+		async initTime() {
+			const dataTime = await fetchTime()
+			const time = getTime(dataTime)
+			const currentTime = getNextTime(time)
+			console.log(currentTime)
+			this._timeLabel.text = currentTime.time
+		}
+
 	})
 
 class Extension {
